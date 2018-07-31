f2b4f6cb4993f8ca6eca4b0e4a342e4b9c96c93c
Anshul kumar <anshul.kumar@daffodilsw.com>
2018-07-31 20:06:12 +0530

    Add migration for tiny url table

--- db/migrate/20180731120917_create_tiny_urls.rb
+++ db/migrate/20180731120917_create_tiny_urls.rb
@@ -1,7 +1,9 @@
 class CreateTinyUrls < ActiveRecord::Migration[5.1]
   def change
     create_table :tiny_urls do |t|
-
+      t.string :orginal_url
+      t.string :generated_url
+      t.text :system_info
 
       t.timestamps
     end
